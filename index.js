e3e67c1e1adb5baf55d13663492f6da5653dd82d
mauger1998 <110930037+mauger1998@users.noreply.github.com>
2022-12-24 19:14:55 +0000

    Name all links and alt tags

---
+++ index.js
@@ -0,0 +1,205 @@
+gsap.registerPlugin(ScrollTrigger)
+
+gsap.to(".secondTitleScroll", {
+    scrollTrigger: {
+        trigger: ".secondTitleScroll",
+        scrub:true,
+        start:"top bottom",
+        end:"+=300",
+        
+    },
+    y:0,
+    opacity:1
+})
+gsap.to(".card img", {
+    scrollTrigger: {
+        trigger: ".card img",
+        scrub:true,
+        start:"top bottom",
+        end:"+=500",
+        
+    },
+    x:0,
+    opacity:1,
+    stagger:0.3,
+})
+gsap.to(".cardScroll", {
+    scrollTrigger: {
+        trigger: ".cardScroll",
+        scrub:true,
+        start:"top bottom",
+        end:"+=100",
+        
+    },
+    y:0,
+    opacity:1,
+    stagger:0.3,
+})
+gsap.to(".productScroll", {
+    scrollTrigger: {
+        trigger: ".productScroll",
+        scrub:true,
+        start:"top bottom",
+        end:"+=500",
+        
+    },
+    x:0,
+    opacity:1,
+    stagger:0.3,
+})
+gsap.to(".productScrollOne", {
+    scrollTrigger: {
+        trigger: ".productScrollOne",
+        scrub:true,
+        start:"top bottom",
+        end:"+=500",
+        
+    },
+    x:0,
+    opacity:1,
+    stagger:0.3,
+})
+gsap.to(".productScrollTwo", {
+    scrollTrigger: {
+        trigger: ".productScrollTwo",
+        scrub:true,
+        start:"top bottom",
+        end:"+=500",
+        
+    },
+    x:0,
+    opacity:1,
+    stagger:0.3,
+})
+gsap.to(".circleScroll", {
+    scrollTrigger: {
+        trigger: ".circleScroll",
+        scrub:true,
+        start:"top bottom",
+        end:"+=500",
+        
+    },
+    x:0,
+    opacity:1,
+    stagger:0.3,
+})
+gsap.to(".circleScrollOne", {
+    scrollTrigger: {
+        trigger: ".circleScrollOne",
+        scrub:true,
+        start:"top bottom",
+        end:"+=500",
+        
+    },
+    x:0,
+    opacity:1,
+    stagger:0.3,
+})
+gsap.to(".circleScrollTwo", {
+    scrollTrigger: {
+        trigger: ".circleScrollTwo",
+        scrub:true,
+        start:"top bottom",
+        end:"+=500",
+        
+    },
+    x:0,
+    opacity:1,
+    stagger:0.3,
+})
+gsap.to(".aboutScroll", {
+    scrollTrigger: {
+        trigger: ".aboutScroll",
+        scrub:true,
+        start:"top bottom",
+        end:"+=500",
+        
+    },
+    x:0,
+    opacity:1,
+    stagger:0.3,
+})
+gsap.to(".right-about", {
+    scrollTrigger: {
+        trigger: ".right-about",
+        scrub:true,
+        start:"top bottom",
+        end:"+=500",
+        
+    },
+    x:0,
+    opacity:1,
+    stagger:0.3,
+})
+gsap.to(".left-values", {
+    scrollTrigger: {
+        trigger: ".left-values",
+        scrub:true,
+        start:"top bottom",
+        end:"+=500",
+        
+    },
+    x:0,
+    opacity:1,
+    stagger:0.3,
+})
+gsap.to(".valueScroll", {
+    scrollTrigger: {
+        trigger: ".valueScroll",
+        scrub:true,
+        start:"top bottom",
+        end:"+=500",
+        
+    },
+    x:0,
+    opacity:1,
+    stagger:0.3,
+})
+gsap.to(".finalScroll", {
+    scrollTrigger: {
+        trigger: ".finalScroll",
+        scrub:true,
+        start:"top bottom",
+        end:"+=500",
+        
+    },
+    x:0,
+    opacity:1,
+    stagger:0.3,
+})
+gsap.to(".right-final", {
+    scrollTrigger: {
+        trigger: ".right-final",
+        scrub:true,
+        start:"top bottom",
+        end:"+=500",
+        
+    },
+    x:0,
+    opacity:1,
+    stagger:0.3,
+})
+gsap.to(".contactScroll", {
+    scrollTrigger: {
+        trigger: ".contactScroll",
+        scrub:true,
+        start:"top bottom",
+        end:"+=500",
+        
+    },
+    x:0,
+    opacity:1,
+    stagger:0.3,
+})
+gsap.to(".contact-section form", {
+    scrollTrigger: {
+        trigger: ".contact-section form",
+        scrub:true,
+        start:"top bottom",
+        end:"+=400",
+        
+    },
+    scale:1,
+    opacity:1,
+    stagger:0.3,
+})
